f7ef29f433fc25677eaaca15cfbd42986c1f0a08
Dimasartha <dimasanto2006@gmail.com>
2025-02-20 22:08:17 +0700

    Add files via upload

---
+++ Soal5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Soal5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String A = scanner.next();
+        String B = scanner.next();
+
+        int totalLength = A.length() + B.length();
+        System.out.println(totalLength);
+
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capitalizedB = B.substring(0, 1).toUpperCase() + B.substring(1);
+
+        System.out.println(capitalizedA + " " + capitalizedB);
+
+        scanner.close();
+    }
+}
